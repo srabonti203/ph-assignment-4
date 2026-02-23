2a8140e9bdc1bb9d6e217ae1658ce2ebfeac9123
srabonti203 <bithisrabontiakter@gmail.com>
2026-02-24 05:40:47 +0600

    initiated git to the project

---
+++ scripts/machine.js
@@ -0,0 +1,5 @@
+// collect ids
+function collectId(id) {
+  element = document.getElementById(id);
+  return element;
+}
---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+// total job count
+let totalCount = collectId("total-count");
+const allCarts = collectId("all-carts");
+totalCount.innerText = allCarts.children.length;
+
+// button toggling
